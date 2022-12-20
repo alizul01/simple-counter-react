6510720928b71b1175931832b6f64a1513b2404f
Ali Zulfikar <62384197+alizul01@users.noreply.github.com>
2022-12-20 08:29:32 +0700

    Create button to handle plus and min counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const handlePlus = () => {
+    setCounter(counter + 1);
+  }
+
+  const handleMin = () => {
+    setCounter(counter - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          fontSize: "100%",
+      }}>
+          <h1>Counter App</h1>
+          <div style={{
+              fontSize: "2rem"
+          }}>
+              {counter}
+          </div>
+
+          <div style={{
+              display: "flex",
+              gap: "1rem",
+          }}>
+              <button style={{
+                  padding: "1rem",
+                  backgroundColor: "green",
+                  fontSize: "1rem",
+                  color: "white",
+                  marginTop: "10vh",
+                  borderRadius: "2rem",
+                  cursor: "pointer",
+                  border: "none"
+              }} onClick={handlePlus}>Increase</button>
+              <button style={{
+                  padding: "1rem",
+                  backgroundColor: "red",
+                  fontSize: "1rem",
+                  color: "white",
+                  marginTop: "10vh",
+                  borderRadius: "2rem",
+                  cursor: "pointer",
+                  border: "none"
+              }} onClick={handleMin}>Decrease</button>
+          </div>
+      </div>
+  )
 }
 
 export default App;
